d4c5c745def62f00f60804a31b727d4649d7338e
VictoRvs23 <victorvidal2201@alumnos.ubiobio.cl>
2026-09-19 12:26:36 -0300

    MOD estudio.routes

--- backend/src/routes/estudio.routes.js
+++ backend/src/routes/estudio.routes.js
@@ -12,6 +12,7 @@ import { uploadImagenEstudio } from "../upload/estudio.upload.js";
 import { validateBody } from "../middleware/validateBody.js";
 import { CrearEstudioValidation, ActualizarEstudioValidation } from "../validations/estudio.validation.js";
 import { handleMulterError } from "../middleware/multerError.middleware.js";
+import disponibilidadRoutes from "./disponibilidad.routes.js";
 
 const router = Router();
 
@@ -23,5 +24,7 @@ router.get("/:id_estudio", getEstudio);
 router.post("/", verifyToken, GestionEstudios, uploadImagenEstudio.single("imagen"),handleMulterError, validateBody(CrearEstudioValidation), createEstudio);
 router.put("/:id_estudio", verifyToken, GestionEstudios, uploadImagenEstudio.single("imagen"), handleMulterError, validateBody(ActualizarEstudioValidation), updateEstudio);
 router.delete("/:id_estudio", verifyToken, GestionEstudios, deleteEstudio);
+
+router.use("/:id_estudio/disponibilidad", disponibilidadRoutes);
 
 export default router;
