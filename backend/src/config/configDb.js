0021918953bf9d1fc7ac7afb808ab404b49d6dde
VictoRvs23 <victorvidal2201@alumnos.ubiobio.cl>
2026-09-25 04:06:26 -0300

    MOD agenda.entity

--- backend/src/config/configDb.js
+++ backend/src/config/configDb.js
@@ -3,7 +3,7 @@ import { DataSource } from "typeorm";
 import { DATABASE, DB_USERNAME, HOST, DB_PASSWORD, DB_PORT } from "./configEnv.js";
 import UsuarioEntity from "../entities/usuario.entity.js"
 import EstudioEntity from "../entities/estudio.entity.js";
-import DisponibilidadEntity from "../entities/disponibilidad.entity.js";
+import DisponibilidadEntity from "../entities/agenda.entity.js";
 import { createUsuarios } from "./InitDb.js";
 
 export const AppDataSource = new DataSource({
