28d338107f748fcf39ba6d6fb1e8690e0d0249b1
VictoRvs23 <victorvidal2201@alumnos.ubiobio.cl>
2026-09-25 04:00:06 -0300

    ADD authorization.middleware & MOD auth.middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -29,14 +29,3 @@ export function verifyToken(req, res, next) {
         });
     }
 }
-
-export const authorizeRoles = (...roles_permitidos) => {
-    return (req, res, next) => {
-        if (!req.usuario || !roles_permitidos.includes(req.usuario.rol)) {
-            return res.status(403).json({ 
-                message: `Acceso denegado: Esta acción requiere rol de ${roles_permitidos.join(" o ")}.` 
-            });
-        }
-        return next();
-    };
-};
